b0b5112f49c5ed6931dd5fd3ff0c5b55e243c32f
moraleswill <105268966+moraleswill@users.noreply.github.com>
2022-06-30 16:19:54 -0400

    added comments and removed requests

--- script.js
+++ script.js
@@ -69,23 +69,3 @@ function getPrompts() {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Get references to the #generate element
-// Write password to the #password input
-// Add event listener to generate button
-
-
-
